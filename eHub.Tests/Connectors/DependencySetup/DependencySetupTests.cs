53c302ca092ead4b2fedeb7006fe248a1f15f2f2
Lukas Jagemann <lukas.jagemann@elementlogic.net>
2025-10-27 10:11:35 +0100

    Fix ISetupDependenciesConnector for inherited types (#32)

--- eHub.Tests/Connectors/DependencySetup/DependencySetupTests.cs
+++ eHub.Tests/Connectors/DependencySetup/DependencySetupTests.cs
@@ -11,33 +11,84 @@ namespace eHub.Tests.Connectors.DependencySetup;
 [TestClass]
 public class DependencySetupTests
 {
-        [TestMethod]
-        public void GivenConnectorImplementsISetupDependenciesConnector_WhenSetupDependenciesCalled_ThenDependenciesAreAvailable()
+    [TestMethod]
+    public void
+        GivenConnectorImplementsISetupDependenciesConnector_WhenSetupDependenciesCalled_ThenDependenciesAreAvailable()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+        var configuration = NullConfiguration.Instance;
+
+        // Act
+        DependencyInjectionFeature.InvokeDependencySetup(typeof(TestConnector), services, configuration);
+
+        // Assert
+        var serviceProvider = services.BuildServiceProvider();
+        var dependency = serviceProvider.GetService<TestDependency>();
+        dependency.Should().NotBeNull();
+        dependency.Value.Should().Be(42);
+    }
+
+    [TestMethod]
+    public void
+        GivenInheritedConnectorWhereBaseImplementsISetupDependenciesConnector_WhenSetupDependenciesCalled_ThenDependenciesAreAvailable()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+        var configuration = NullConfiguration.Instance;
+
+        // Act
+        DependencyInjectionFeature.InvokeDependencySetup(typeof(InheritedTestConnectorWithoutRedeclaration), services,
+            configuration);
+
+        // Assert
+        var serviceProvider = services.BuildServiceProvider();
+        var dependency = serviceProvider.GetService<TestDependency>();
+        dependency.Should().NotBeNull();
+        dependency.Value.Should().Be(42);
+    }
+
+    [TestMethod]
+    public void
+        GivenInheritedConnectorWhereConnectorReImplementsISetupDependenciesConnector_WhenSetupDependenciesCalled_ThenDependenciesAreAvailable()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+        var configuration = NullConfiguration.Instance;
+
+        // Act
+        DependencyInjectionFeature.InvokeDependencySetup(typeof(InheritedTestConnectorWithRedeclaration), services,
+            configuration);
+
+        // Assert
+        var serviceProvider = services.BuildServiceProvider();
+        var dependency = serviceProvider.GetService<TestDependency>();
+        dependency.Should().NotBeNull();
+        dependency.Value.Should().Be(100);
+    }
+
+    private class TestConnector : ISetupDependenciesConnector
+    {
+        public event UpdateStatusDelegate? UpdateStatus;
+        public Task Run(CancellationToken cancellationToken) => Task.CompletedTask;
+
+        public static void SetupDependencies(ConnectorServicesBuilder builder)
         {
-            // Arrange
-            var services = new ServiceCollection();
-            var configuration = NullConfiguration.Instance;
-
-            // Act
-            DependencyInjectionFeature.InvokeDependencySetup(typeof(TestConnector), services, configuration);
-
-            // Assert
-            var serviceProvider = services.BuildServiceProvider();
-            var dependency = serviceProvider.GetService<TestDependency>();
-            dependency.Should().NotBeNull();
-            dependency.Value.Should().Be(42);
+            builder.Services.AddSingleton(new TestDependency(42));
         }
+    }
 
-        class TestConnector : ISetupDependenciesConnector
+    private class InheritedTestConnectorWithoutRedeclaration : TestConnector
+    {
+    }
+
+    private class InheritedTestConnectorWithRedeclaration : TestConnector, ISetupDependenciesConnector
+    {
+        public new static void SetupDependencies(ConnectorServicesBuilder builder)
         {
-            public event UpdateStatusDelegate? UpdateStatus;
-            public Task Run(CancellationToken cancellationToken) => Task.CompletedTask;
-
-            public static void SetupDependencies(ConnectorServicesBuilder builder)
-            {
-                builder.Services.AddSingleton(new TestDependency(42));
-            }
+            builder.Services.AddSingleton(new TestDependency(100));
         }
+    }
 
-        record TestDependency(int Value);
+    private record TestDependency(int Value);
 }
--- eHub/Scripting/Connectors/Features/DependencyInjectionFeature.cs
+++ eHub/Scripting/Connectors/Features/DependencyInjectionFeature.cs
@@ -4,9 +4,10 @@ using eHub.PlugIn.Configuration;
 
 namespace eHub.Scripting.Connectors.Features;
 
-public class DependencyInjectionFeature
+public static class DependencyInjectionFeature
 {
-    public static void InvokeDependencySetup(Type connectorType, IServiceCollection connectorServices, IConfiguration configuration)
+    public static void InvokeDependencySetup(Type connectorType, IServiceCollection connectorServices,
+        IConfiguration configuration)
     {
         if (!TypeUtilities.IsDependencySetup(connectorType))
         {
@@ -14,8 +15,23 @@ public class DependencyInjectionFeature
         }
 
         var builder = new ConnectorServicesBuilder(connectorServices, configuration);
-        var setupMethod = connectorType.GetMethod(nameof(ISetupDependenciesConnector.SetupDependencies),
-            BindingFlags.Static | BindingFlags.Public, [typeof(ConnectorServicesBuilder)])!;
-        setupMethod.Invoke(null, [builder]);
+
+        var traverseType = connectorType;
+        while (traverseType != null && traverseType != typeof(object))
+        {
+            var setupMethod = traverseType.GetMethod(nameof(ISetupDependenciesConnector.SetupDependencies),
+                BindingFlags.Static | BindingFlags.Public, [typeof(ConnectorServicesBuilder)]);
+
+            if (setupMethod != null)
+            {
+                setupMethod.Invoke(null, [builder]);
+                return;
+            }
+
+            traverseType = traverseType.BaseType;
+        }
+
+        throw new InvalidOperationException(
+            $"No static public method '{nameof(ISetupDependenciesConnector.SetupDependencies)}' found in type hierarchy of '{connectorType.FullName}'");
     }
 }
